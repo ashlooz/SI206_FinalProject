e2a40e9ad525831b1f7f7a39304e57e71d984bfd
ashlooz <97359169+ashlooz@users.noreply.github.com>
2023-12-11 21:06:22 -0500

    Update visualizations.py

--- visualizations.py
+++ visualizations.py
@@ -1,35 +1,50 @@
-from spotify_lastfm_database import (
-    setup_database,
-    spotify_data_retrieval,
-    lastfm_data_retrieval,
-    create_artist_and_song_tables,
-    insert_data_into_tables)
+import sqlite3
+import matplotlib.pyplot as plt
 
-from visualizations import plot_artist_distribution, popularity_valence_visual
+# visual representation of the data:
 
-def main():
-    # lastfm api key
-    lastfm_api_key = "5d2702b8f89733c46c584f393263c35c"
-    # set up database
-    cursor, connection = setup_database("billboard_hot_100.db")
-    
-    # make tables
-    create_artist_and_song_tables(cursor, connection)
+# valence = vibes (0.0 - 1.0)
+# (0.0 = sad/bad vibes, 1.0 = happy/upbeat vibes)
+# popularity = how popular a song is (0 - 100)
 
-    # get data from spotify and lastfm
-    track_data, valence, danceability, energy = spotify_data_retrieval()
-    
-    # insert data into tables
-    insert_data_into_tables(track_data, valence, danceability, energy, cursor, connection, lastfm_api_key)
-    
-    # plot data for top 15 artists with most songs
-    plot_artist_distribution(cursor)
+# plots the distribution of songs on the billboard hot 100
+# compares the popularity of the song to the valence (mood/vibes) of the song
+# question this plot addresses: do songs with a higher valence value tend to be more popular?
+def popularity_valence_visual(cursor):
+   # retrieve the popularity and valence of each song from song table
+   cursor.execute("SELECT popularity, valence FROM Song")
+   features = cursor.fetchall()
+   popularities, valences = zip(*features)
 
-    # plot data for song popularity vs valence
-    popularity_valence_visual(cursor)
+   plt.scatter(popularities, valences, color='salmon')
+   plt.xlabel('Popularity')
+   plt.ylabel('Valence')
+   plt.title('Song Popularity vs Valence')
+   plt.grid(True)
 
-    # close the connection to database
-    connection.close()
+   plt.show()
 
-if __name__ == "__main__":
-    main()
+# plots the distribution of artists (only to 15 shown) on the billboard hot 100
+def plot_artist_distribution(cursor):
+   # sets a limit on the query to the top 15 artists with the most songs on hot 100 playlist
+   cursor.execute("""
+       SELECT Artist.name, COUNT(Song.id)
+       FROM Song
+       JOIN Artist ON Song.artist_id = Artist.id
+       GROUP BY Artist.id
+       ORDER BY COUNT(Song.id) DESC
+       LIMIT 15
+   """)
+   artist_data = cursor.fetchall()
+
+   # unpacking the fetched data
+   artists, song_counts = zip(*artist_data)
+
+   # plotting the data on chart (bar chart with to 15 artists)
+   plt.bar(artists, song_counts, color='salmon')
+   plt.xlabel('Artists')
+   plt.ylabel('Number of Songs')
+   plt.xticks(rotation=90)
+   plt.title('Top 15 Artists by Number of Songs on Billboard Hot 100')
+
+   plt.show()
